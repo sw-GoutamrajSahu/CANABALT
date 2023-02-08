eb795a725f37f83809e34e87614b404af0a2c3b2
Goutamraj Sahu <119305388+sw-GoutamrajSahu@users.noreply.github.com>
2023-02-08 17:45:56 +0530

    Initial Object

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float gravity;
+    public Vector2 velocity;
+    public float jumpVelocity = 20;
+    public float groundHeight = 10;
+    public bool isGrounded = false;
+    public bool isHoldingJump = false;
+    public float maxHoldJumpTime = 0.4f;
+    public float holdJumpTimer = 0.0f;
+    public float jumpGroundThreshold = 1;
+
+    void Update()
+    {
+        Vector2 pos = transform.position;
+        float groundDistance = Mathf.Abs(pos.y - groundHeight);
+
+        if (isGrounded || groundDistance <= jumpGroundThreshold)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                isGrounded = false;
+                velocity.y = jumpVelocity;
+                isHoldingJump = true;
+                holdJumpTimer = 0;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isHoldingJump = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        if (!isGrounded)
+        {
+            if (isHoldingJump)
+            {
+                holdJumpTimer += Time.fixedDeltaTime;
+                if (holdJumpTimer >= maxHoldJumpTime)
+                {
+                    isHoldingJump = false;
+                }
+            }
+            pos.y += velocity.y * Time.fixedDeltaTime;
+            if (!isHoldingJump)
+            {
+                velocity.y += gravity * Time.fixedDeltaTime;
+            }
+
+            if (pos.y <= groundHeight)
+            {
+                pos.y = groundHeight;
+                isGrounded = true;
+            }
+        }
+        transform.position = pos;
+    }
+
+}
